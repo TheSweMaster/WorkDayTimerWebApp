038077898ece7642d3fec701451f0251faaef86f
Jacob Gandemo <theswemaster@gmail.com>
2019-12-30 18:30:40 +0100

    Add WorkDayTimer Migration

--- WorkDayTimer/Data/Migrations/20191230172035_Add_WorkDayTimer.Designer.cs
+++ WorkDayTimer/Data/Migrations/20191230172035_Add_WorkDayTimer.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkDayTimerWebApp.Data;
 
 namespace WorkDayTimerWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191230172035_Add_WorkDayTimer")]
+    partial class Add_WorkDayTimer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WorkDayTimer/Data/Migrations/20191230172035_Add_WorkDayTimer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WorkDayTimerWebApp.Data.Migrations
+{
+    public partial class Add_WorkDayTimer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkDayTimers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    WorkDayHours = table.Column<int>(nullable: false),
+                    TimerTime = table.Column<DateTime>(nullable: false),
+                    IsRunning = table.Column<bool>(nullable: false),
+                    MaxTimerCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkDayTimers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkDayTimers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDayTimers_UserId",
+                table: "WorkDayTimers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkDayTimers");
+        }
+    }
+}
